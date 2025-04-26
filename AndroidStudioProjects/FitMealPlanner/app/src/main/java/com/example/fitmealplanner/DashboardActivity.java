75bb7c1d6f7b1d412b4f65acacd99cc54aaee0ed
hAbraha <h.gebrekidan428@mybvc.ca>
2025-04-26 00:20:08 -0600

    Fix bottom navigation and dashboard activity

--- AndroidStudioProjects/FitMealPlanner/app/src/main/java/com/example/fitmealplanner/DashboardActivity.java
+++ AndroidStudioProjects/FitMealPlanner/app/src/main/java/com/example/fitmealplanner/DashboardActivity.java
@@ -2,7 +2,10 @@ package com.example.fitmealplanner;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class DashboardActivity extends AppCompatActivity {
 
@@ -10,5 +13,23 @@ public class DashboardActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+
+        BottomNavigationView bottomNav = findViewById(R.id.bottomNavigation);
+
+        bottomNav.setOnItemSelectedListener(item -> {
+                    int id = item.getItemId();
+
+                    if (id == R.id.nav_dashboard) {
+                        return true;
+                    } else if (id == R.id.nav_meal) {
+                        startActivity(new Intent(DashboardActivity.this, MealPlannerActivity.class));
+                        return true;
+                    } else if (id == R.id.nav_nutrition) {
+                        startActivity(new Intent(DashboardActivity.this, NutritionLookupActivity.class));
+                        return true;
+                    }
+
+                    return  false;
+        });
     }
 }
